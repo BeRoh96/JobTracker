bf48b9df892b9bbd30728cd21ead74115efb60b1
B96R7 <bennetrohwer.123@gmail.com>
2023-02-23 13:51:17 +0100

    setup register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+import { Logo } from "../components";
+import styled from "styled-components";
+import { useState } from "react";
+
+const inititialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(inititialState);
+
+  const handleChange = (e: React.FormEvent) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+      </form>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  display: grid;
+  align-items: center;
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h3 {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+  }
+  .btn {
+    margin-top: 1rem;
+  }
+  .member-btn {
+    background: transparent;
+    border: transparent;
+    color: var(--primary-500);
+    cursor: pointer;
+    letter-spacing: var(--letterSpacing);
+  }
+`;
 
 export default Register;
